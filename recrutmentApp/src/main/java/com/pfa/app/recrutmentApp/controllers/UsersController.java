301179c5ad21b7c2f60f3f6f3131510ffbafa7b4
Ntono Mathieu Fistele <mathieu.ntono@iit.ens.tn>
2020-03-16 00:28:56 +0100

    class pour la gestion des utilisateurs

--- recrutmentApp/src/main/java/com/pfa/app/recrutmentApp/controllers/UsersController.java
+++ recrutmentApp/src/main/java/com/pfa/app/recrutmentApp/controllers/UsersController.java
@@ -1,4 +1,14 @@
 package com.pfa.app.recrutmentApp.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userRegistration")
 public class UsersController {
+
+    public void addUser(){
+
+    }
+
 }
