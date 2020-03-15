56de11c9ba43ef49bcafe0e51180bf6ed295874b
Ntono Mathieu Fistele <mathieu.ntono@iit.ens.tn>
2020-03-15 16:27:43 +0100

    mise a jour du controller

--- recrutmentApp/src/main/java/com/pfa/app/recrutmentApp/controllers/RecrutementController.java
+++ recrutmentApp/src/main/java/com/pfa/app/recrutmentApp/controllers/RecrutementController.java
@@ -1,5 +1,12 @@
 package com.pfa.app.recrutmentApp.controllers;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/recruitment/connect")
 public class RecrutementController {
-
+    @RequestMapping(value =  "/checkConnection", method = RequestMethod.GET)
+    public boolean connect(String username, String password){
+        return true;
+    }
 }
