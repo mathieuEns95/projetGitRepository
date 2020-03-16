3377b97ebaf95938a9c4c518d155b73cd9d80f06
Ntono Mathieu Fistele <mathieu.ntono@iit.ens.tn>
2020-03-16 13:05:45 +0100

    ajout des fonctions d'enregistrement

--- recrutmentApp/src/main/java/com/pfa/app/recrutmentApp/controllers/RecrutementController.java
+++ recrutmentApp/src/main/java/com/pfa/app/recrutmentApp/controllers/RecrutementController.java
@@ -3,11 +3,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-@RequestMapping("/recruitment/connect")
+@RequestMapping("/recrutment/connect")
 public class RecrutementController {
     @RequestMapping(value =  "/checkConnection", method = RequestMethod.GET)
     public boolean connect(String username, String password){
-        return true;
+        if (username == "admin" && password == "root"){
+            return true;
+        }else {
+            return false;
+        }
     }
-
 }
--- recrutmentApp/src/main/java/com/pfa/app/recrutmentApp/controllers/UsersController.java
+++ recrutmentApp/src/main/java/com/pfa/app/recrutmentApp/controllers/UsersController.java
@@ -1,14 +1,38 @@
 package com.pfa.app.recrutmentApp.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/userRegistration")
+@RequestMapping("/recrutment/register")
 public class UsersController {
+    @RequestMapping(value = "/userRegistration", method = RequestMethod.GET)
+    public String addCandidate(String id, String firstName, String lastName,
+                          String adress, String email, String nationalite,
+                          int phone, String gender, String birthday, String pasword){
 
-    public void addUser(){
+        return "id : "+id+
+                "\n first name : " +firstName+
+                "\n last name : "+lastName+
+                "\n address : "+adress+
+                "\n email : "+email+
+                "\n nationality : "+nationalite+
+                "\n phone : "+phone+
+                "\n gender : "+gender+
+                "\n birthday : "+birthday+
+                "\n password : "+pasword;
+    }
 
+    public String addAdmin(String id, String firstName, String lastName, String email,
+                           String gender, String password){
+
+        return "id : "+id+
+                "\n first name : "+firstName+
+                "\n last name : "+lastName+
+                "\n email : "+email+
+                "\n gender : "+gender+
+                "\n password : "+password;
     }
 
 }
