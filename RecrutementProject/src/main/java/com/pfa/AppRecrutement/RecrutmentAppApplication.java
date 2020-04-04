589f5e6797134a3ed715a6afe758539139f387f6
Manel benrached <manelbenrached11@gmail.com>
2020-04-05 00:50:19 +0100

    mise à jour de projet avec connexion spring boot, spring security , base de donnée et angular

---
+++ RecrutementProject/src/main/java/com/pfa/AppRecrutement/RecrutmentAppApplication.java
@@ -0,0 +1,22 @@
+package com.pfa.AppRecrutement;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+
+public class RecrutmentAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecrutmentAppApplication.class, args);
+    }
+
+}
+
+
+
+
+
---
+++ RecrutementProject/src/main/java/com/pfa/AppRecrutement/email/MailConfig.java
@@ -0,0 +1,31 @@
+package com.pfa.AppRecrutement.email;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("pfagroupe3@gmail.com");
+        mailSender.setPassword("PFA12345");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+}
