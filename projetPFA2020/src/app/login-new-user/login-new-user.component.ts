ed24790e18702d1b78156d069b19cbe41ad8b912
Cedric Lonla <cmoffolonla@gmail.com>
2020-04-27 11:14:27 +0100

    Update login-new-user.component.ts

--- projetPFA2020/src/app/login-new-user/login-new-user.component.ts
+++ projetPFA2020/src/app/login-new-user/login-new-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AccessService} from '../services/access.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-new-user',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginNewUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private access: AccessService, private router: Router) { }
+  private candidate: any;
 
   ngOnInit() {
+
   }
 
+  onSaveCandidates(data: any) {
+    this.access.saveResource(this.access.host + "/candidates", data)
+      .subscribe(data => {
+        this.candidate = data;
+
+      }, error => {
+        console.log(error);
+      });
+
+  }
 }
