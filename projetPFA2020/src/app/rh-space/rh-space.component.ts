3faaf5d494d61c3ab8ecafe464c1f9cda36fb211
Cedric Lonla <cmoffolonla@gmail.com>
2020-04-27 11:14:38 +0100

    Update rh-space.component.ts

--- projetPFA2020/src/app/rh-space/rh-space.component.ts
+++ projetPFA2020/src/app/rh-space/rh-space.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AccessService} from "../services/access.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-rh-space',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rh-space.component.css']
 })
 export class RhSpaceComponent implements OnInit {
+  candidates: any;
 
-  constructor() { }
+  constructor(private access: AccessService, private router: Router) {
+  }
 
   ngOnInit() {
   }
 
+  afficherCandidates() {
+    this.access.getResource("http://localhost:8080/candidates")
+      .subscribe(data => {
+        this.candidates = data;
+
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  onSendEmail(c: any) {
+    
+  }
 }
